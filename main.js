d049f408f9583d415d45d4610e9742a7aa90f675
b-chavez <brandedhard@gmail.com>
2020-10-17 10:10:12 -0700

    fixed function

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var scheduledDay = document.querySelector("#scheduled-day");
 var week = document.querySelector(".week");
-week.addEventListener('click', function(event){
+week.addEventListener('click', changeDay)
+
+function changeDay(event){
     scheduledDay.innerHTML = "Scheduled Events for " + event.target.innerHTML;
-})
+}
