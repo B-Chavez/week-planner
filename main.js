1077c989e576847a612cbfdcfe5754ec9a9d79be
b-chavez <brandedhard@gmail.com>
2020-10-17 14:38:42 -0700

    User can add entry to day, modal and js added

--- main.js
+++ main.js
@@ -8,3 +8,58 @@ function changeDay(event){
     }
     
 }
+
+var button = document.querySelector("button");
+button.addEventListener('click', displayModal);
+var submit = document.querySelector("#submit");
+submit.addEventListener('click', function (){
+    displayModal()
+    submitFunc()
+})
+
+var showModal = false;
+function displayModal(){
+    var modal = document.querySelector(".modal");
+    var transparentBG = document.querySelector(".transparentBG");
+    if(showModal === false){
+        modal.classList.remove("hidden");
+        transparentBG.classList.remove("hidden");
+        showModal = true;
+    } else {
+        modal.classList.add("hidden");
+        transparentBG.classList.add("hidden");
+        showModal = false;
+    }
+}
+
+var descCount = 0;
+var submitText = document.querySelector("#entry");
+var description = document.querySelectorAll(".description");
+var time = document.querySelectorAll(".time");
+var dayOption = document.querySelector("#day");
+var timeOption = document.querySelector("#time")
+
+function submitFunc(){
+    
+
+    if(submitText.value === "" || dayOption.selectedIndex === 0 || timeOption.selectedIndex === 0){
+        resetEntry()
+        return;
+    }
+
+    description[descCount].textContent = submitText.value;
+    time[descCount].textContent = dayOption[dayOption.selectedIndex].innerHTML + " " + timeOption[timeOption.selectedIndex].innerHTML;
+    
+    resetEntry()
+    submitText.value = "";
+    descCount++;
+    if(descCount === 6){
+        descCount = 0;
+    }
+}
+
+function resetEntry(){
+    dayOption.selectedIndex = 0;
+    timeOption.selectedIndex = 0;
+    submitText.value = "";
+}
